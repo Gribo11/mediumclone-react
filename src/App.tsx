a297a71fef949efb4395e531548e0642842eb7b5
Sergey Kuzmenko <s.kuzmenko@sense.pro>
2023-04-06 15:58:46 +0300

    add base profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { FC } from 'react';
-import {Header} from './common/components/header/header.component';
-import {Banner} from './common/components/banner/banner.component';
-import { Article } from './modules/feed/components/article/article.component';
-import { Feed } from './modules/feed/components/feed/feed.component';
-import { ArticleList } from './modules/feed/components/article-list/article-list.component';
+import { FC } from "react";
+import { Header } from "./common/components/header/header.component";
+import { Route, Routes } from "react-router-dom";
+import { GlobalFeedPage } from './modules/feed/pages/global-feed.page';
+import { ProfilePage } from './modules/profile/pages/profile.page';
 
 interface AppProps {}
 
 export const App: FC<AppProps> = () => {
- 
-
   return (
     <div className="pb-16">
-     <Header/>
-     <Banner/>
-     <Feed/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<GlobalFeedPage />} />
+        <Route path="/:profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 };
